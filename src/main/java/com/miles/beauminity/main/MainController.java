59a0bf7a1e3a10eaa365c25880c25cc39bde240e
nincat <emily_1204@naver.com>
2026-06-16 10:19:16 +0900

    modified:   src/main/java/com/miles/beauminity/main/MainController.java 	modified:   src/main/resources/templates/main/index.html

--- src/main/java/com/miles/beauminity/main/MainController.java
+++ src/main/java/com/miles/beauminity/main/MainController.java
@@ -2,13 +2,43 @@ package com.miles.beauminity.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MainController {
     
+    // 메인화면 이동
     @GetMapping("/")
     public String getMainHome() {
         return "main/index";
     }
+
+    // 후기 게시판 이동
+    @GetMapping("/board/review")
+    public String getReviewList() {
+        return "review_board/review";
+    }
+
+    // 질문 게시판 이동
+    @GetMapping("/board/qna")
+    public String getQnaList() {
+        return "qna_board/qna";
+    }
+
+    // 정보공유 게시판 이동
+    @GetMapping("/board/info")
+    public String getInfoShareList() {
+        return "infoshare_board/info";
+    }
+
+    // 피드 게시판 이동
+    @GetMapping("/feed")
+    public String getFeedList() {
+        return "feed/list";
+    }
+
+    // 
+    
     
 }
