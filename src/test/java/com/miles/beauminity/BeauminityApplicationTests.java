7b6a518845bb46bdf55f016c89ffd98b6425fccd
soonhyun <tns0332@naver.com>
2026-06-16 09:24:26 +0900

    First commit

---
+++ src/test/java/com/miles/beauminity/BeauminityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miles.beauminity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeauminityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
