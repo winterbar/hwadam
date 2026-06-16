f2eb9f08978b3395c525d23656a9e2a46a34dd57
soonhyun <tns0332@naver.com>
2026-06-16 17:31:06 +0900

    feat:메인화면 내 로그인 영역 수정, 로그인 화면 구현, 회원가입 화면 구현, 아이디 중복체크 구현

--- src/main/java/com/miles/beauminity/controller/login/MemberController.java
+++ src/main/java/com/miles/beauminity/controller/login/MemberController.java
@@ -2,6 +2,8 @@ package com.miles.beauminity.controller.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -10,5 +12,14 @@ public class MemberController {
     public String getRegisterPage() {
         return "login/register";
     }
+
+
+    @GetMapping("/chkid-dup/{username}")
+    @ResponseBody
+    public boolean checkIdDuplicate(@PathVariable("username") String username) {
+        boolean isDuplicate = false;
+        return isDuplicate;
+    }
+    
     
 }
