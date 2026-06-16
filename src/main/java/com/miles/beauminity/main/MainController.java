6b34c8ae8ffd6ddc112e63fa343299bc8229f7d2
nincat <emily_1204@naver.com>
2026-06-16 12:39:22 +0900

    modified:   src/main/java/com/miles/beauminity/main/MainController.java 	modified:   src/main/resources/templates/qna_board/qna.html 	deleted:    src/main/java/com/miles/beauminity/controller/qna_board/.gitkeep 	deleted:    src/main/resources/static/css/write.css

--- src/main/java/com/miles/beauminity/main/MainController.java
+++ src/main/java/com/miles/beauminity/main/MainController.java
@@ -2,7 +2,6 @@ package com.miles.beauminity.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
