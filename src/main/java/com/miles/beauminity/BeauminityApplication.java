8646806c7cd92a81d2d74e73d67d74cc5939bf05
soonhyun <tns0332@naver.com>
2026-06-16 09:24:26 +0900

    First commit

---
+++ src/main/java/com/miles/beauminity/BeauminityApplication.java
@@ -0,0 +1,13 @@
+package com.miles.beauminity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeauminityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeauminityApplication.class, args);
+	}
+
+}
