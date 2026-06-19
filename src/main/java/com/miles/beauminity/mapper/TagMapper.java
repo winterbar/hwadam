70ee7ca3ac32cc8e93711282262c75178e54652f
박재은 <jill031018@gmail.com>
2026-06-19 17:28:40 +0900

    feat : 피드 삭제 기능 추가

--- src/main/java/com/miles/beauminity/mapper/TagMapper.java
+++ src/main/java/com/miles/beauminity/mapper/TagMapper.java
@@ -5,12 +5,21 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.miles.beauminity.vo.feed.FeedTagVO;
 import com.miles.beauminity.vo.feed.TagVO;
 
 @Mapper
 public interface TagMapper {
     void postTag(TagVO tagNames);
+
     List<String> getTagNameList();
-    void tagFeed(@Param("feedId")Long getFeedId,@Param("tagId") Long getTagId);
+
+    List<String> getFeedTagList(Long feedId);
+
+    void tagFeed(@Param("feedId") Long getFeedId, @Param("tagId") Long getTagId);
+
     List<String> getfeedTagList();
+
+    void deleteTag(Long feedId);
+
 }
