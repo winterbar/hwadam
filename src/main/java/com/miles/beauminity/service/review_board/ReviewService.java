bb6ca680fa50378bbf0cee5467d4e3b59af5b187
jehyeong <jehyeong@example.com>
2026-06-19 17:32:52 +0900

    feat: 후기 게시글 전체조회 기능 구현 완료. 현재 게시글 상세보기 구현중

--- src/main/java/com/miles/beauminity/service/review_board/ReviewService.java
+++ src/main/java/com/miles/beauminity/service/review_board/ReviewService.java
@@ -7,10 +7,14 @@ import com.miles.beauminity.vo.review.ReviewBoardVO;
 public interface ReviewService { // 역할: 후기 게시판에 대한 서비스 요청 처리
 
     // 후기 게시글 등록 명세
-    void registerReviewPost(ReviewBoardVO reviewBoardVO);
+    boolean registerReviewPost(ReviewBoardVO reviewBoardVO);
     
     // 후기 게시판 목록 조회 명세
     List<ReviewBoardVO> getReviewBoardList();
+
+    // 후기 게시팜 목록에서 게시글 상세조회 명세
+    ReviewBoardVO getReviewBoardDetail(Long boardId);
+
         
 }
 
