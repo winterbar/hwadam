a9edff10b81c77d44d5a4d228680166817709dac
soonhyun <tns0332@naver.com>
2026-06-23 17:03:42 +0900

    feat:회원 정보 수정 구현 완료, 비밀번호 변경 구현 완료

--- src/main/resources/static/js/mypage/info.js
+++ src/main/resources/static/js/mypage/info.js
@@ -1,6 +1,8 @@
 // 프로필 편집 버튼을 눌렀을 때 모달을 여는 기능
 function openProfileModal() {
     const modal = document.getElementById('profileModal');
+    document.getElementById('fileInfo').style.display = 'none';
+    document.getElementById('saveBtn').disabled = true;
     if (modal) {
         modal.style.display = 'flex';
         saveBtn.disabled = true;
@@ -23,6 +25,61 @@ function closeProfileModal() {
         document.getElementById('fileName').textContent = "";
         document.getElementById('fileSize').textContent = "";
     }
+}
+
+// 회원정보 수정, 비밀번호 변경을 눌렀을 때 모달을 여는 기능
+function openPasswordModal(url) {
+    document.getElementById('passwordConfirmModal').style.display = 'flex';
+    document.getElementById('path').value = url; // 목적지 설정
+}
+
+// 확인 버튼을 눌렀을 때 모달을 닫는 기능
+function closePasswordModal() {
+    document.getElementById('passwordConfirmModal').style.display = 'none';
+    document.getElementById('passwordForm').reset();
+    document.getElementById('passwordError').style.display = 'none';
+}
+
+// ESC 키로 모달을 닫는 기능
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape' || event.keyCode === 27) {
+        const profileModal = document.getElementById('profileModal');
+        const passwordConfirmModal = document.getElementById('passwordConfirmModal');
+        
+        if (profileModal.style.display === 'flex') {
+            closeProfileModal();
+        }
+        
+        if (passwordConfirmModal.style.display === 'flex') {
+            closePasswordModal();
+        }
+    }
+});
+
+// 비밀번호 불일치 시, 페이지가 로드되면 모달을 실행
+window.onload = function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get('error') === 'true') { // 비밀번호가 맞지 않다면 에러가 발생
+        document.getElementById('passwordConfirmModal').style.display = 'flex';
+        document.getElementById('passwordError').style.display = 'block';
+
+        const savedUrl = urlParams.get('path');
+        if(savedUrl) {
+            document.getElementById('path').value = savedUrl;
+        }
+    }
+};
+
+// 수정 성공 여부를 알려주는 토스트 알림
+function showToast(message) {
+    const toast = document.getElementById("toast");
+    toast.textContent = message;
+    toast.className = "toast show";
+    
+    // 3초 뒤에 토스트 숨김
+    setTimeout(function() {
+        toast.className = toast.className.replace("show", "");
+    }, 3000);
 }
 
 // 스크립트 실행 전 브라우저가 모두 로딩될 때까지 기다림
