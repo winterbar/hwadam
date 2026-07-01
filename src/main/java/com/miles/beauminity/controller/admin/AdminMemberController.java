3581a10ca0eb4f634f11c1bde718b051c7a5e502
soonhyun <tns0332@naver.com>
2026-07-01 17:36:35 +0900

    feat:메인화면 관리자모드 버튼 추가 완료, 관리자 페이지 화면 구현 chore:회원 관리 페이지 화면 구현중

---
+++ src/main/java/com/miles/beauminity/controller/admin/AdminMemberController.java
@@ -0,0 +1,11 @@
+package com.miles.beauminity.controller.admin;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class AdminMemberController {
+    
+}
