38a03f506e3e27ea2c32df40c739b88c5ebfb7c8
soonhyun <tns0332@naver.com>
2026-07-08 19:50:48 +0900

    feat:다중 필터링 포인트 조건 구현, 검색 이후에도 필터링 조건이 유지되도록 변경 및 이에 따른 페이징, 칼럼별 정렬 수정, 통계 화면 추가 완료

--- src/main/java/com/miles/beauminity/service/admin/AdminMemberServiceImpl.java
+++ src/main/java/com/miles/beauminity/service/admin/AdminMemberServiceImpl.java
@@ -1,14 +1,18 @@
 package com.miles.beauminity.service.admin;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.miles.beauminity.mapper.admin.AdminMemberMapper;
-import com.miles.beauminity.vo.admin.AdminMemberAnalysisVO;
 import com.miles.beauminity.vo.admin.AdminMemberBatchVO;
+import com.miles.beauminity.vo.admin.AdminMemberConditionVO;
+import com.miles.beauminity.vo.admin.AdminMemberStatsVO;
 import com.miles.beauminity.vo.admin.AdminMemberVO;
 import com.miles.beauminity.vo.admin.AdminPageVO;
 
@@ -30,36 +34,27 @@ public class AdminMemberServiceImpl implements AdminMemberService {
 
     // 회원 통계 필터링 결과 회원 수 (페이징에 사용)
     @Override
-    public long countAnalysisMember(AdminMemberAnalysisVO adminMemberAnalysisVO) {
-        return adminMemberMapper.countAnalysisMember(adminMemberAnalysisVO);
+    public long countAnalysisMember(AdminMemberConditionVO adminMemberConditionVO) {
+        return adminMemberMapper.countAnalysisMember(adminMemberConditionVO);
     }
 
     // 회원 조회 필터링
     @Override
     public List<AdminMemberVO> searchMembers(AdminPageVO adminPageVO) {
         // 검색어가 null일 경우 DB에서 검색하지 않음
+        System.out.println(adminPageVO.toString());
         if (adminPageVO.getKeyword() == null) {
             return null;
         }
-        // 정렬 기준 칼럼이 null이 아닐 경우
-        if (adminPageVO.getSort() != null) {
-            switch (adminPageVO.getSort()) {
-                case "1":
-                    adminPageVO.setSort("username");
-                    break;
-                case "2":
-                    adminPageVO.setSort("name");
-                    break;
-                case "3":
-                    adminPageVO.setSort("nickname");
-                    break;
-                case "4":
-                    adminPageVO.setSort("point");
-                    break;
-                case "5":
-                    adminPageVO.setSort("signed_at");
-                    break;
-            }
+        // 만약 정렬 기준이 일치하지 않으면 정렬을 수행하지 않음
+        if ((adminPageVO.getSort() != null) && 
+            (!"username".equals(adminPageVO.getSort()) &&
+             !"name".equals(adminPageVO.getSort()) &&
+             !"nickname".equals(adminPageVO.getSort()) &&
+             !"point".equals(adminPageVO.getSort()) &&
+             !"signedAt".equals(adminPageVO.getSort()))
+        ) {
+            return null;
         }
         // searchType이 all일 경우 전체 목록을 검색
         // searchType이 있고 검색어가 있을 경우 조건에 따라 검색 (단, 키워드가 빈칸이면 검색X)
@@ -70,30 +65,34 @@ public class AdminMemberServiceImpl implements AdminMemberService {
     
     // 회원 분석 필터링
     @Override
-    public List<AdminMemberVO> analysisMembers(AdminMemberAnalysisVO adminMemberAnalysisVO, AdminPageVO adminPageVO) {
-        // 정렬 기준 칼럼이 null이 아닐 경우
-        if (adminPageVO.getSort() != null) {
-            switch (adminPageVO.getSort()) {
-                case "1":
-                    adminPageVO.setSort("username");
-                    break;
-                case "2":
-                    adminPageVO.setSort("name");
-                    break;
-                case "3":
-                    adminPageVO.setSort("nickname");
-                    break;
-                case "4":
-                    adminPageVO.setSort("point");
-                    break;
-                case "5":
-                    adminPageVO.setSort("signed_at");
-                    break;
-            }
-        }
-        List<AdminMemberVO> members = adminMemberMapper.selectAnalysisMembers(adminMemberAnalysisVO, adminPageVO);
+    public List<AdminMemberVO> analysisMembers(AdminMemberConditionVO adminMemberConditionVO, AdminPageVO adminPageVO) {
+        List<AdminMemberVO> members = adminMemberMapper.selectAnalysisMembers(adminMemberConditionVO, adminPageVO);
         convertText(members);
         return members;
+    }
+
+    // 회원 분석 필터링 결과 통계
+    @Override
+    public AdminMemberStatsVO statsMembers(AdminMemberConditionVO adminMemberConditionVO) {
+        List<AdminMemberVO> members = adminMemberMapper.selectAnalysisAllMember(adminMemberConditionVO);
+        
+        Map<String, Long> genderStats = members.stream()
+            .filter(m -> m.getGender() != null)
+            .collect(Collectors.groupingBy(AdminMemberVO::getGender, Collectors.counting()));
+        Map<String, Long> roleStats = members.stream()
+            .filter(m -> m.getRole() != null)
+            .collect(Collectors.groupingBy(AdminMemberVO::getRole, Collectors.counting()));
+        Map<String, Long> statusStats = members.stream()
+            .filter(m -> m.getStatus() != null)
+            .collect(Collectors.groupingBy(AdminMemberVO::getStatus, Collectors.counting()));
+        Map<String, Long> skinTypeStats = members.stream()
+            .filter(m -> m.getSkinType() != null)
+            .collect(Collectors.groupingBy(AdminMemberVO::getSkinType, Collectors.counting()));
+        Map<String, Long> personalColorStats = members.stream()
+            .filter(m -> m.getPersonalColor() != null)
+            .collect(Collectors.groupingBy(AdminMemberVO::getPersonalColor, Collectors.counting()));
+        
+        return AdminMemberStatsVO.of(genderStats, roleStats, statusStats, skinTypeStats, personalColorStats);
     }
 
     // 화면에 출력하기 위한 텍스트 변경
