1edd3ebd4c91f006779fdfcb811775dae9076435
HHelloGuys <jill031018@gamil.com>
2026-07-12 23:30:19 +0900

    feat: 네이버 쇼핑 API 연동 및 상품 목록 화면 출력

---
+++ src/main/java/com/miles/beauminity/service/store/StoreService.java
@@ -0,0 +1,12 @@
+package com.miles.beauminity.service.store;
+
+import java.util.List;
+
+import com.miles.beauminity.vo.store.StoreVO;
+
+
+public interface StoreService {
+ List<StoreVO> getStoreList();
+ void getNaverProducts(String keyword);
+    
+}
