12d63c219c4d948cfb490257e317708ca912f1c4
nincat <emily_1204@naver.com>
2026-07-14 12:24:50 +0900

    feat:로그인하지 않은 사용자의 댓글 제한 추가

--- src/main/resources/static/js/qna/qna_check_id.js
+++ src/main/resources/static/js/qna/qna_check_id.js
@@ -2,12 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const buttonField = document.getElementById('action-field');
     const boardId = buttonField.dataset.id;
+    const commentArea = document.getElementById('comment-content');
+    const commentBtn = document.getElementById('comment-submit');
+    
 
     fetch(`/api/board/${boardId}/check-id`, { method: 'GET' })
         .then(res => res.json())
         .then(data => {
             if (data.isOwner === false) {
                 buttonField.style.display = "none";
+            }
+            if (data.isLogOn === false){
+                commentArea.value="로그인하시고 댓글을 작성해보세요!"
+                commentArea.disabled = true;
             }
         })
 });
